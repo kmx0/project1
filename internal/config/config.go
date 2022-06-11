edcbce6a8da26aa9707e4be6e1470343c061787d
kmx0 <kamoxa0@yandex.ru>
2022-06-11 16:32:43 +0300

    8080

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Config struct {
-	Address    string `env:"ADDRESS" envDefault:"127.0.0.1:8081"`
+	Address    string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
 	Key        string `env:"KEY" `
 	DBURI      string `env:"DATABASE_URI" envDefault:"postgres://postgres:postgres@localhost:5432/gophermart"`
 	AccSysSddr string `env:"ACCRUAL_SYSTEM_ADDRESS" envDefault:"http://127.0.0.1:8080"`
